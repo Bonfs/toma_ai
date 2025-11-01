7c971d721b314bb5bc1b76b2a2e9cf4d083fda0f
Matheus Bonfim <matheusbonfim@MacBook-Pro-de-Matheus.local>
2025-11-01 00:44:34 -0300

    first commit

---
+++ libraries/design_system/build.gradle.kts
@@ -0,0 +1,45 @@
+plugins {
+    alias(libs.plugins.android.library)
+    alias(libs.plugins.kotlin.android)
+    alias(libs.plugins.kotlin.compose)
+}
+
+android {
+    namespace = "com.example.puc.design_system"
+    compileSdk {
+        version = release(36)
+    }
+
+    defaultConfig {
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.toVersion(libs.versions.java.get())
+        targetCompatibility = JavaVersion.toVersion(libs.versions.java.get())
+    }
+    kotlinOptions {
+        jvmTarget = libs.versions.java.get()
+    }
+    buildFeatures {
+        compose = true
+    }
+}
+
+dependencies {
+    implementation(libs.androidx.activity.compose)
+    implementation(platform(libs.androidx.compose.bom))
+    implementation(libs.androidx.compose.ui)
+    implementation(libs.androidx.compose.ui.graphics)
+    implementation(libs.androidx.compose.ui.tooling.preview)
+    implementation(libs.androidx.compose.material3)
+}
---
+++ settings.gradle.kts
@@ -0,0 +1,46 @@
+pluginManagement {
+    repositories {
+        google {
+            content {
+                includeGroupByRegex("com\\.android.*")
+                includeGroupByRegex("com\\.google.*")
+                includeGroupByRegex("androidx.*")
+            }
+        }
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "Toma ai!"
+include(":app")
+//include(":libraries")
+//include(":features")
+
+fun includeProjects(directory: File, path: String, maxDepth: Int = 1) {
+    directory.listFiles().orEmpty().also { it.sort() }.forEach { file ->
+        if (file.isDirectory) {
+            val newPath = "$path:${file.name}"
+            val buildFile = File(file, "build.gradle.kts")
+            if (buildFile.exists()) {
+                include(newPath)
+                logger.lifecycle("Included project: $newPath")
+            } else if (maxDepth > 0) {
+                includeProjects(file, newPath, maxDepth - 1)
+            }
+        }
+    }
+}
+
+//include(":features:onboarding")
+//include(":libraries:design_system")
+//include(":libraries:configs")
+includeProjects(File(rootDir, "features"), ":features")
+includeProjects(File(rootDir, "libraries"), ":libraries")
